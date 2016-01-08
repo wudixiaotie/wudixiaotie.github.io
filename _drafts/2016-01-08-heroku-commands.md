2a0f0ecd4e73a22f2a7f3a33de950433cf167a5d
xiaotie <wudixiaotie@gmail.com>
2016-01-08 11:12:53 +0800

    new post

---
+++ _drafts/2016-01-08-heroku-commands.md
@@ -0,0 +1,16 @@
+---
+layout: post
+title:  "Heroku commands"
+author: "肖铁(Kevin)"
+categories: heroku
+---
+
+##Check how many dynos are running using the ps command:
+{% highlight console %}
+heroku ps
+{% endhighlight %}
+
+##Run command:
+{% highlight console %}
+heroku run
+{% endhighlight %}
--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 $(function() {
-    // traffic();
+    traffic();
 });
 
 // 访问量统计
 function traffic() {
+    referer_url = document.referrer;
+    referer_domain = referer_url.split('/')[2];
     $.ajax({
-        url: "",
+        url: "http://localhost:3000/visitors",
+        type: "post",
+        data: {
+            referer: referer_domain
+        }
     });
 }
